7dc49c7fffe0988043be872db7ad0b0cb1f85a39
Jannick <jannick.bloemendal@gmail.com>
2024-04-29 15:53:48 +0200

    lcd scherm en stiekem ook vernier

---
+++ src/main-MacBook Air van Jannick.h
@@ -0,0 +1,19 @@
+/*
+ * file: main.h
+ * Header file
+ *
+ * authors:
+ *  Jannick Bloemendal
+ *  Niels Redegeld
+ *  Thijs Vader
+ *  Rutger Jansen
+ *
+ * Hogeschool Utrecht
+ * Date: 29-04-2024
+ *
+ * Version: 1.1.0
+ *
+ * CHANGELOG:
+ *
+ *
+ */
---
+++ src/main.h
@@ -0,0 +1,28 @@
+/*
+ * file: main.h
+ * Header file
+ *
+ * authors:
+ *  Jannick Bloemendal
+ *  Niels Redegeld
+ *  Thijs Vader
+ *  Rutger Jansen
+ *
+ * Hogeschool Utrecht
+ * Date: 29-04-2024
+ *
+ * Version: 1.1.0
+ *
+ * CHANGELOG:
+ *
+ *
+ */
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h> // LCD SCREEN
+#include <Servo.h>
+#include <VernierLib.h> //include Vernier functions
+
+// button debounce lib
+// UART serialplotter lib?
