a0da057bfef742aa86cff3a318d368e2721527e0
Jannick <jannick.bloemendal@gmail.com>
2024-04-30 19:24:23 +0200

    commit

--- src/main.h
+++ src/main.h
@@ -27,24 +27,24 @@
 #include <LiquidCrystal_I2C.h> // LCD SCREEN
 #include <Servo.h>
 #include <VernierLib.h> //include Vernier functions
-#include <Bounce2.h> // button debounce lib
+#include <Bounce2.h>    // button debounce lib
 // UART serialplotter lib?
 
 // buttons
-#define NUM_BTNS 3 
+#define NUM_BTNS 3
 
 // MOTOR test parameters
 #define CYCLES 500
 #define STEPS 9
 #define MINIMUM_THRUST 1500
-#define THRUST_LADDER  50
+#define THRUST_LADDER 50
 #define DUR_PROG_A 20
 #define DUR_PROG_B 10000
-#define THRUST_LADDER  50
+#define THRUST_LADDER 50
 
-// measurement 
-#define VOLTS_ADC_STEP 20/1024
-#define AMS_ADC_Step 20/1024
+// measurement
+#define VOLTS_ADC_STEP 20 / 1024
+#define AMS_ADC_Step 20 / 1024
 
 struct measurement // structure containing the measurements
 {
@@ -53,10 +53,14 @@ struct measurement // structure containing the measurements
     float power;
 };
 
-typedef struct measurement  MEASUREMENT;  // MEASUREMENT  == struct 
-typedef MEASUREMENT*        PMEASUREMENT; // PMEASUREMENT == struct measurement*
+typedef struct measurement MEASUREMENT; // MEASUREMENT  == struct
+typedef MEASUREMENT        *PMEASUREMENT;      // PMEASUREMENT == struct measurement*
 
-enum testPrograms{ A, B }; // testprograms enumerator
+enum testPrograms
+{
+    A,
+    B
+}; // testprograms enumerator
 
 void armESC();
 int readVernier();
