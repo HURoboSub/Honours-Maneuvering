8e7a937c4c122a3bec0d7afd19e5a13dd1d7d273
Jannick <jannick.bloemendal@gmail.com>
2024-05-16 16:53:54 +0200

    Added test code to use with compiler

--- test/testStateMachine.cpp
+++ test/testStateMachine.cpp
@@ -1,168 +1,115 @@
-#include "unity.h"
+/*  testStateMachine.cpp
+    Author: J. Bloemendal
+    Template
+    Date: xx-xx-2024
+    Test states using a gcc or dev cpp compilor
+    To compile:
+        OS X build:
+            g++ testStateMachine.cpp -o runme -std=c++11xw
+/ // ADD (*) to start of this line TO toggle code
 
-#include "main.h" // main header file
+#include <iostream>
 
-#define DEBUG // (Serial) DEBUG mode (un)comment to toggle
+// Defines
+// #define DEBUG               // Uncomment to enable DEBUG mode
 
-/* Timing configuration */
-const int timeBtwnReadings = 500; // time between Vernierr
-unsigned long lastReadTime = 0ul;
+#define LCD_ROWS 2
+#define LCD_COLS 16
 
-LiquidCrystal_I2C lcd(LCD_addr, LCD_COLS, LCD_ROWS); // set the LCD address to LCD_addr for a LCD_chars by LCD_lines display
+// enumator for the system states
+enum class systemState
+{
+    Setup,       // S0 Init setup state
+    Calibrating, // S1
+    Reading,     // S2
+    Testing,     // S3
+    Output       // S4
+} currentState;  // class storing the current system state
 
-char const *rowOneLCD = "               ";   // const pointer to a char array containing linezero of LCD
-char const *rowTwoLCD = "               "; // textrow one of LCD
+const char *rowOneLCD = "              "; // max 16 chars
+const char *rowTwoLCD = "              "; // max 16 chars
 
-VernierLib Vernier; // create an instance of the VernierLib library
-
-/* PIN DEFINTIONS */
-uint8_t VOLT_PIN = A0; // Define Voltage control
-uint8_t AMP_PIN = A1;  // Define Amperage control
-
-uint8_t ESC_PIN = 3; // Define ESC control pin
-
-const uint8_t BUTTON_PINS[NUM_BUTTONS] = {4, 7, 8}; // Define ESC control pin D4 D7 D8
-Bounce *buttons = new Bounce[NUM_BUTTONS];          // Initiate 3 Bounce objects
-bool buttonStates[NUM_BUTTONS] = {false};           // bool array storing the buttonStates
-bool *pButtonStates = &buttonStates[0];             // define pointer, pointing to zeroth element of buttonStates array
-
-Servo esc; // Create a Servo object
-
-MEASUREMENT data;           // measurement data
-PMEASUREMENT pData = &data; // point to datastrucutre
-
-enum testPrograms testProgram = A; // default to test program A
-
-void setUp(void) {
-  // set stuff up here
-}
-
-void tearDown(void) {
-  // clean stuff up here
-}
-
-/*
-  Function: LCD_show
-    Handles the lcd display (clear, cursor and print) based on LCD_ROWS and LCD_COLS
-  Parameter: pointer to a string in a 2d
- */
+void LCD_show(char **str);
+void userInterface(systemState cState); // Displays the systemstate on the LCD scren
 
 void LCD_show(char **str)
 {
-  unsigned char x,y; // x and y loop index
+    unsigned char x, y; // y loop index
 
-  lcd.clear(); // clear the display
+    for (y = 0; y < LCD_ROWS; y++)
+    {
+        for (x = 0; x < LCD_COLS; x++)
+        {
+            str[0][x] = rowOneLCD[x]; // copy row one to stirng buffer
+            str[1][x] = rowTwoLCD[x]; // copy row two to stirng buffer
+        }
+    }
 
-  // for each row copy textlines to dispText char arrays
-  for (y = 0; y < LCD_ROWS; y++)
-  {
-      for (x = 0; x < LCD_COLS; x++)
-      {
-          str[0][x] = rowOneLCD[x];
-          str[1][x] = rowTwoLCD[x];
-      }
-  }
-
-  // Display the contents of the display buffer on the LCD screen
-  for (y = 0; y < LCD_ROWS; y++, str++)
-  {
-    lcd.setCursor(0, y);
-    lcd.print(*str); // print the string buffer
-  }
+    for (y = 0; y < LCD_ROWS; y++, str++)
+    {
+        std::cout << *str << '\n';
+    }
 }
-
-/*
-  Function: userInterface
-    Handles different system states on the LCD screen
-    Parameters: class enumator with the current State
- */
 
 void userInterface(systemState cState)
 {
-  unsigned char y; // y loop index
+    unsigned char y; // y loop index
 
-  // Memory allocation for the display text
-  char **dispText = new char *[LCD_ROWS];
+    char **dispText = new char *[LCD_ROWS];
 
-  for (y = 0; y < LCD_ROWS; y++)
-  {
-    dispText[y] = new char[LCD_COLS];
-  }
+    for (y = 0; y < LCD_ROWS; y++)
+    {
+        dispText[y] = new char[LCD_COLS];
+    }
 
-  switch (cState)
-  {
-  case systemState::Setup:
-    // Setup state here
-    rowOneLCD = "S: Setup       ";
-    break;
-  case systemState::Calibrating:
-    // Calibrating state here
-    rowOneLCD = "S: Calibrating ";
-    break;
-  case systemState::Reading:
-    // S2 here
-    rowOneLCD = "S: Reading     ";
-    break;
-  case systemState::Testing:
-    // S3 Testing here
-    rowOneLCD = "S: Testing     ";
-    break;
-  case systemState::Output:
-    // S4 Output here
-    rowOneLCD = "S: Output      ";
-    break;
-  default: // Should never get in default state 
-    rowOneLCD = "S: Error       ";
-    rowTwoLCD = "No state passed";
-    break;
-  }
+    switch (currentState)
+    {
+    case systemState::Setup:
+        // Setup state here
+        rowOneLCD = "S: Setup       ";
+        break;
+    case systemState::Calibrating:
+        // Calibrating state here
+        rowOneLCD = "S: Calibrating ";
+        break;
+    case systemState::Reading:
+        // S2 here
+        rowOneLCD = "S: Reading     ";
+        break;
+    case systemState::Testing:
+        // S3 Testing here
+        rowOneLCD = "S: Testing     ";
+        break;
+    case systemState::Output:
+        // S4 Output here
+        rowOneLCD = "S: Output      ";
+        break;
+    default:
+        rowOneLCD = "S: Error       ";
+        rowTwoLCD = "No state passed";
+        break;
+    }
 
-  // Show the updated display
-  LCD_show(dispText);
+    // Show the initial display
+    LCD_show(dispText);
 
-  // delete allocated memory for dispText buffer 
-  for (y = 0; y < LCD_ROWS; y++)
-  {
-    delete[] dispText[y];
-  }
-  delete[] dispText;
-
+    // Free allocated memory
+    for (y = 0; y < LCD_ROWS; y++)
+    {
+        delete[] dispText[y];
+    }
+    delete[] dispText;
 }
 
-void test_states(void) {
-    // test each state
+int main(int argc, char *argv[])
+{
+    // test each states
     for (int state = 0; state < 5; state++)
     {
         currentState = static_cast<systemState>(state);
         userInterface(currentState);
     }
+    return 0;
 }
-
-int runUnityTests(void) {
-  UNITY_BEGIN();
-  RUN_TEST(test_states);
-  return UNITY_END();
-}
-
-// WARNING!!! PLEASE REMOVE UNNECESSARY MAIN IMPLEMENTATIONS //
-
-/**
-  * For native dev-platform or for some embedded frameworks
-  */
-int main(void) {
-  return runUnityTests();
-}
-
-/**
-  * For Arduino framework
-  */
-void setup() {
-  // Wait ~2 seconds before the Unity test runner
-  // establishes connection with a board Serial interface
-  delay(2000);
-
-  runUnityTests();
-}
-void loop() {
-    
-}
+/*
+*/
